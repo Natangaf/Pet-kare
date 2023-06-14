e876cfa6dfb7fa5aa98845a158cf11560cc7a786
natabgaf <natanfernandes52@gmail.com>
2023-06-14 04:00:55 -0300

    feat: tarefa 2 finalizada

--- traits/models.py
+++ traits/models.py
@@ -1,8 +1,10 @@
 from django.db import models
+from django.utils import timezone
 
 
 class Trait(models.Model):
-    name = models.CharField(max_length=20,unique=True)
+    name = models.CharField(max_length=20, unique=True)
+    created_at = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
         return self.name
