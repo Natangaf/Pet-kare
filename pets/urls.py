0527a66d129c2519b326a53e34cb67e7f4754bb8
natabgaf <natanfernandes52@gmail.com>
2023-06-16 13:38:10 -0300

    feat: tarefa 4 finalizada

--- pets/urls.py
+++ pets/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import petsView
+from .views import petsView ,petsDetailView
 
 urlpatterns = [
     path('pets/', petsView.as_view()),
+    path('pets/<int:pets_id>/', petsDetailView.as_view()),
 ]
--- pets/views.py
+++ pets/views.py
@@ -4,11 +4,19 @@ from .models import Pet
 from .serializers import PetSerializer
 from groups.models import Group
 from traits.models import Trait
+from django.shortcuts import get_object_or_404
 
 
 class petsView(APIView, CustomPageNumberPagination):
     def get(self, req: Request) -> Response:
-        pets = Pet.objects.all()
+        trait_params = req.query_params.get("trait", None)
+        # trait = Trait.objects.filter(name__iexact=trait_params).first()
+
+        if trait_params:
+            pets = Pet.objects.filter(traits__name=trait_params)
+        else:
+            pets = Pet.objects.all().order_by("id")
+
         result_page = self.paginate_queryset(pets, req, view=self)
         serializer = PetSerializer(result_page, many=True)
 
@@ -22,20 +30,79 @@ class petsView(APIView, CustomPageNumberPagination):
         traits_data = serializer.validated_data.pop("traits")
         pet_data = serializer.validated_data
 
-        group = Group.objects.filter(scientific_name__iexact= group_data["scientific_name"]).first()
+        group = Group.objects.filter(
+            scientific_name__iexact=group_data["scientific_name"]
+        ).first()
 
         if not group:
             group = Group.objects.create(**group_data)
 
         instance_pets: Pet = Pet.objects.create(**pet_data, group=group)
 
+        trait_list= []
         for trait_data in traits_data:
             trait = Trait.objects.filter(name__iexact=trait_data["name"]).first()
             if not trait:
-                trait = Trait.objects.create(**trait_data)
-            instance_pets.traits.add(trait)
+                trait = Trait.objects.create(**trait_data)    
+            trait_list.append(trait)
+
+        instance_pets.traits.set(trait_list)
         instance_pets.save()
 
         response = PetSerializer(instance_pets)
 
-        return Response(response.data,status.HTTP_201_CREATED)
+        return Response(response.data, status.HTTP_201_CREATED)
+
+
+class petsDetailView(APIView, CustomPageNumberPagination):
+    def get (self, req: Request, pets_id: int) -> Response:
+
+        pet = get_object_or_404(Pet, id=pets_id)
+
+        respose = PetSerializer(instance=pet)
+
+        return Response(data=respose.data, status=status.HTTP_200_OK)
+    
+    def patch(self, req: Request, pets_id: int) -> Response:
+
+        pet = get_object_or_404(Pet, id=pets_id)
+
+        serializer = PetSerializer(data=req.data ,partial=True)
+
+        serializer.is_valid(raise_exception=True)
+
+        group_data = serializer.validated_data.pop("group",None)
+        traits_data = serializer.validated_data.pop("traits",None)
+
+        for key, value in serializer.validated_data.items():
+            setattr(pet, key, value)
+
+        if group_data:
+            group = Group.objects.filter(
+                scientific_name__iexact=group_data["scientific_name"]
+            ).first()
+            if not group:
+                group = Group.objects.create(**group_data)
+            pet.group = group
+
+
+        if traits_data:
+            trait_list= []
+            for trait_data in traits_data:
+                trait = Trait.objects.filter(name__iexact=trait_data["name"]).first()
+                if not trait:
+                    trait = Trait.objects.create(**trait_data)    
+                trait_list.append(trait)
+
+            pet.traits.set(trait_list)
+
+        pet.save()
+
+        respose = PetSerializer(instance=pet)
+
+        return Response(data=respose.data, status=status.HTTP_200_OK)
+
+    def delete(self, req: Request, pets_id: int) -> Response:
+        pet = get_object_or_404(Pet, id=pets_id)
+        pet.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
